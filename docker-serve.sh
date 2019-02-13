54b76b676c34dd1b9de09f51c14ed207f72cb4a2
Joanna Dabal <joanna@9ci.com>
2019-02-13 16:49:57 -0600

    how to build docker first

--- docker-serve.sh
+++ docker-serve.sh
@@ -1,7 +1,11 @@
+#first build docker (if still not published): docker build --tag=9cigithubio . 
+
+
+
 #!/usr/bin/env bash
 
 set -e
 
 #for local runs put your docker name
-docker run -it --rm --name=9cicomlocal-serve --volume="$PWD:/srv/jekyll" -p 3000:3000 -p 3001:3001 52d875e04ef1 /bin/sh install-run.sh
+docker run -it --rm --name=9cicomlocal-serve --volume="$PWD:/srv/jekyll" -p 3000:3000 -p 3001:3001 9cigithubio /bin/sh install-run.sh
 
